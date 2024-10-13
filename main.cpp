334bb3e8b473a80b40f5d35f271738af9a5f7701
SheikhUbaid9 <ubaidf31@gmail.com>
2024-10-13 19:54:57 +0500

    add binary search

--- main.cpp
+++ main.cpp
@@ -13,6 +13,27 @@ void bubbleSort(int arr[], int n) {
         }
     }
 }
+int binarySearch(int arr[], int left, int right, int key) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        // Check if key is at mid
+        if (arr[mid] == key) {
+            return mid;
+        }
+
+        // If key is greater, ignore the left half
+        if (arr[mid] < key) {
+            left = mid + 1;
+        }
+        // If key is smaller, ignore the right half
+        else {
+            right = mid - 1;
+        }
+    }
+    // Key not present in the array
+    return -1;
+}
 int main()
 {
  
